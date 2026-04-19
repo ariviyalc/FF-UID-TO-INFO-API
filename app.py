26e29ac62361cc00b7cdff1cd24030f9232c8101
ariviyalc <ariviyal@outlook.com>
2026-04-19 09:44:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,5 +163,5 @@ async def startup():
 
 if __name__ == '__main__':
     asyncio.run(startup())
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
